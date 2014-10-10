45e6d9ba4865f9e92960e951a3b27e32d9bbc906
Rachel Reese <rach@rachelree.se>
2014-10-09 23:23:48 -0400

    Updates to database.

--- Data.fs
+++ Data.fs
@@ -5,11 +5,14 @@ open System.IO
 
 module Data = 
     [<Literal>]
-    let private connectionString = @"Data Source=" + __SOURCE_DIRECTORY__ + @"/Resources/task.sqlite;Version=3;" 
+    let private connectionString = @"Data Source=" + 
+                                    __SOURCE_DIRECTORY__ + 
+                                    @"/Resources/task.sqlite;Version=3;" 
 
     type sql = SqlDataProvider<ConnectionString = connectionString,
                                DatabaseVendor = Common.DatabaseProviderTypes.SQLITE,
-                               ResolutionPath = @"/Library/Frameworks/Mono.framework/Libraries/mono/4.5/",
+                               ResolutionPath = // Mono.Data.SqliteClient location
+                                   @"/Library/Frameworks/Mono.framework/Libraries/mono/4.5/",
                                UseOptionTypes = false>
 
     type task = {Id : Int64; Description : string; mutable Complete : bool}
